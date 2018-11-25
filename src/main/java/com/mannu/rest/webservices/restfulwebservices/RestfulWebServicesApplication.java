bc1923434473d750b0b4d958397f5269b6425046
Manpreet <machopra.work10@gmail.com>
2018-11-25 19:17:38 +0530

    Implementing Internatioanlization

--- src/main/java/com/mannu/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/mannu/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,7 +1,14 @@
 package com.mannu.rest.webservices.restfulwebservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -9,4 +16,19 @@ public class RestfulWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	/*@Bean
+	public ResourceBundleMessageSource bundleMessageSource(){
+		ResourceBundleMessageSource messageSource= new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}*/
+
 }
